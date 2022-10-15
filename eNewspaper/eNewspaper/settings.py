128f2a80f690f3f90f0d1493659c8de1cbf072cb
Tümer Altunbaş <tumeraltunbass@gmail.com>
2022-10-15 14:39:17 +0300

    Update settings.py

--- eNewspaper/eNewspaper/settings.py
+++ eNewspaper/eNewspaper/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+9=51&_@8%t^)j*=ty(pgi39#y%i$h!u-smbbt_p@!1fitzw%z'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
